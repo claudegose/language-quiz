066585c3ce9aa8b2fee1430d1dc99005d5d1b121
claudegose <84382636+claudegose@users.noreply.github.com>
2021-08-09 21:06:51 +0200

    Added Array_Push to Class LanguageGame

--- Starter-pack/classes/LanguageGame.php
+++ Starter-pack/classes/LanguageGame.php
@@ -2,15 +2,16 @@
 
 class LanguageGame
 {
-    private array $words;
+    private array $words = [];
+    public string $answer;
+    public Word $randomWord;
 
     public function __construct()
     {
-        // :: is used for static functions
-        // They can be called without an instance of that class being created
-        // and are used mostly for more *static* types of data (a fixed set of translations in this case)
+
         foreach (Data::words() as $frenchTranslation => $englishTranslation) {
-            // TODO: create instances of the Word class to be added to the words array
+          $word = new Word($frenchTranslation, $englishTranslation);
+          array_push($this->words, $word);
         }
     }
 
