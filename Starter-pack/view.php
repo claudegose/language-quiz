943783f8415ab6dd7514f63c9ed34afb8f36fd1c
claudegose <84382636+claudegose@users.noreply.github.com>
2021-08-09 14:31:50 +0200

    Establishing Uplink

---
+++ Starter-pack/view.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css">
+    <title>THE ONE AND ONLY GAME!</title>
+</head>
+<body>
+
+
+<h1>Ready? Lets play!</h1>
+	<!-- TODO: add a form for the user to play the game -->
+
+<div class="row">
+    <div class="col">
+        <input type="text" class="form-control" aria-label="First name">
+    </div>
+    <div class="col">
+        <input type="text" class="form-control" placeholder="Submit Your Answer" aria-label="Last name">
+
+    </div>
+</div>
+
+    <div class="btm">
+        <button>Submit</button>
+    </div>
+
+</body>
+</html>
